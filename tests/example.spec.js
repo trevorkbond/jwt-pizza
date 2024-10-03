24f095652a22da1c7edacdfa14cfdd84a4b7b24d
Trevor Bond <trevorkbond@gmail.com>
2024-10-03 16:26:56 -0600

    Mock login endpoint

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,17 +6,34 @@ test("home page", async ({ page }) => {
   expect(await page.title()).toBe("JWT Pizza");
 });
 
-test("login", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+test("login with diner use", async ({ page }) => {
+  await page.route("*/**/api/auth", async (route) => {
+    const loginReq = { email: "d@jwt.com", password: "a" };
+    const loginRes = {
+      user: {
+        id: 3,
+        name: "Kai Chen",
+        email: "d@jwt.com",
+        roles: [{ role: "diner" }],
+      },
+      token: "abcdef",
+    };
+    expect(route.request().method()).toBe("PUT");
+    expect(route.request().postDataJSON()).toMatchObject(loginReq);
+    await route.fulfill({ json: loginRes });
+  });
+
+  await page.goto("/");
+
   await page.getByRole("link", { name: "Login" }).click();
-  await page.getByPlaceholder("Email address").fill("test@jwt.com");
+  await page.getByPlaceholder("Email address").fill("d@jwt.com");
   await page.getByPlaceholder("Email address").press("Tab");
-  await page.getByPlaceholder("Password").fill("test");
+  await page.getByPlaceholder("Password").fill("a");
   await page.getByPlaceholder("Password").press("Enter");
   await expect(
-    page.getByRole("link", { name: "t", exact: true })
+    page.getByRole("link", { name: "KC", exact: true })
   ).toBeVisible();
-  await page.getByRole("link", { name: "t", exact: true }).click();
-  await expect(page.getByRole("main")).toContainText("test@jwt.com");
+  await page.getByRole("link", { name: "KC", exact: true }).click();
+  await expect(page.getByRole("main")).toContainText("d@jwt.com");
   await expect(page.getByRole("main")).toContainText("diner");
 });
