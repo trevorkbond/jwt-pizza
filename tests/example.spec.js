387229fdb87f98df747e5d718553236b3e99e405
Trevor Bond <trevorkbond@gmail.com>
2024-10-03 16:06:47 -0600

    Add recorded login test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,3 +5,18 @@ test("home page", async ({ page }) => {
 
   expect(await page.title()).toBe("JWT Pizza");
 });
+
+test("login", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByPlaceholder("Email address").fill("test@jwt.com");
+  await page.getByPlaceholder("Email address").press("Tab");
+  await page.getByPlaceholder("Password").fill("test");
+  await page.getByPlaceholder("Password").press("Enter");
+  await expect(
+    page.getByRole("link", { name: "t", exact: true })
+  ).toBeVisible();
+  await page.getByRole("link", { name: "t", exact: true }).click();
+  await expect(page.getByRole("main")).toContainText("test@jwt.com");
+  await expect(page.getByRole("main")).toContainText("diner");
+});
