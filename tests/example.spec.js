1de230284e06da1b7b8414299649008c3b83d706
Trevor Bond <trevorkbond@gmail.com>
2024-10-09 13:22:36 -0600

    Make login testing fancy and modularized

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from "playwright-test-coverage";
+import { mockedUsers } from "./mockedUsers";
 
 test("home page", async ({ page }) => {
   await page.goto("/");
@@ -6,34 +7,44 @@ test("home page", async ({ page }) => {
   expect(await page.title()).toBe("JWT Pizza");
 });
 
-test("login with diner use", async ({ page }) => {
+// TODO: create standard set of mocked requests
+
+async function mockAuthEndpoint(page, user) {
   await page.route("*/**/api/auth", async (route) => {
-    const loginReq = { email: "d@jwt.com", password: "a" };
-    const loginRes = {
-      user: {
-        id: 3,
-        name: "Kai Chen",
-        email: "d@jwt.com",
-        roles: [{ role: "diner" }],
-      },
-      token: "abcdef",
-    };
+    const loginReq = user.loginReq;
+    const loginRes = user.loginRes;
     expect(route.request().method()).toBe("PUT");
     expect(route.request().postDataJSON()).toMatchObject(loginReq);
     await route.fulfill({ json: loginRes });
   });
+}
+
+async function testLogin(page, user) {
+  await mockAuthEndpoint(page, user);
 
   await page.goto("/");
 
   await page.getByRole("link", { name: "Login" }).click();
-  await page.getByPlaceholder("Email address").fill("d@jwt.com");
+  await page.getByPlaceholder("Email address").fill(user.loginReq.email);
   await page.getByPlaceholder("Email address").press("Tab");
-  await page.getByPlaceholder("Password").fill("a");
+  await page.getByPlaceholder("Password").fill(user.loginReq.password);
   await page.getByPlaceholder("Password").press("Enter");
   await expect(
-    page.getByRole("link", { name: "KC", exact: true })
+    page.getByRole("link", { name: user.profileInitials, exact: true })
   ).toBeVisible();
-  await page.getByRole("link", { name: "KC", exact: true }).click();
-  await expect(page.getByRole("main")).toContainText("d@jwt.com");
-  await expect(page.getByRole("main")).toContainText("diner");
+  await page
+    .getByRole("link", { name: user.profileInitials, exact: true })
+    .click();
+  await expect(page.getByRole("main")).toContainText(user.loginReq.email);
+  await expect(page.getByRole("main")).toContainText(
+    user.loginRes.user.roles[0].role
+  );
+}
+
+test("login with diner user", async ({ page }) => {
+  await testLogin(page, mockedUsers.diner);
+});
+
+test("login with admin user", async ({ page }) => {
+  await testLogin(page, mockedUsers.admin);
 });
---
+++ tests/mockedUsers.js
@@ -0,0 +1,28 @@
+export const mockedUsers = {
+  diner: {
+    loginReq: { email: "d@jwt.com", password: "a" },
+    loginRes: {
+      user: {
+        id: 3,
+        name: "Kai Chen",
+        email: "d@jwt.com",
+        roles: [{ role: "diner" }],
+      },
+      token: "dinerToken",
+    },
+    profileInitials: "KC",
+  },
+  admin: {
+    loginReq: { email: "admin@jwt.com", password: "admin" },
+    loginRes: {
+      user: {
+        id: 4,
+        name: "Admin Guy",
+        email: "admin@jwt.com",
+        roles: [{ role: "admin" }],
+      },
+      token: "adminToken",
+    },
+    profileInitials: "AG",
+  },
+};
